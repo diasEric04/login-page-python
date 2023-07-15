9247d812ea4e1393cd9c6a04448b4f74f0df24de
eric dias <ericarrudadias@gmail.com>
2023-07-15 10:28:24 -0300

    inicializando a home

---
+++ main/views/main_views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Home'
+    }
+    return render(
+        request,
+        'main/index.html',
+        context
+    )
